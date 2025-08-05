b9fc12395b8a2336bf195451af89a1bba0914076
EhabAmawi <ehabamawi98@gmail.com>
2025-08-05 23:08:38 +0300

    add migration script for Categories table

---
+++ prisma/migrations/20250805200754_add_categories_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "public"."categories" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "is_active" BOOLEAN NOT NULL DEFAULT true,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_name_key" ON "public"."categories"("name");
